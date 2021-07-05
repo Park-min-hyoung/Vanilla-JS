34bc4c6ba162f1afe4579791391aa110fadb556a
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-05 15:56:07 +0900

    event.preventDefault()를 이용해 link의 기본동작 막기

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form")
 const ioginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     console.log(ioginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick)
