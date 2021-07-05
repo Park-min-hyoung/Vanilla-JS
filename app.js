846199fa4a6d287b64be487107c5cde5ed2b4e0c
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-05 16:51:09 +0900

    localStorage의 user 정보 유무에 따라 Form 또는 인사말 출력

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const ioginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = ioginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username); // input(text)로 부터 user 정보를 받는다
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null) { // localStoage에 user 정보가 없을 때
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(saveUsername); // localStoage로 부터 user 정보를 받는다
+}
