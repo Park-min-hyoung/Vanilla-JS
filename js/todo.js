311ce68018fdc9d446ee74c9cf20bdb20ad62912
Park-min-hyoung <qkralswgud1324@naver.com>
2021-07-10 16:14:39 +0900

    toDoList 항목 삭제 기능 구현

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const todoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span"); 
-    li.appendChild(span); // span을 li의 자식으로
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span); // span을 li의 자식으로
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
